d9122bf72184136ef88bbfc2a8848cd90c50bf3a
Spebell <sebastian@die-sperlinge.de>
2026-05-03 10:02:35 +0200

    Update installation script for internet-autostart

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd
-git clone https://github.com/Spebelgenenst/internet-connection-checker
-cd internet-connection-checker/
-chmod +x internet-connection-checker.desktop
-mv internet-connection-checker.desktop ~/.config/autostart/
-echo to configure run "nano ~/internet-connection-checker/config.json" uwu
+git clone https://github.com/Spebelgenenst/internet-autostart
+cd internet-autostart/
+chmod +x internet-autostart.desktop
+mv internet-autostart.desktop ~/.config/autostart/
+echo to configure run "nano ~/internet-autostart/config.json" uwu
